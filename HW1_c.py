03934d277351f3cb0ef8cf9627a7a22ab70af59f
Mai Bui <buim@cua.edu>
2020-09-09 22:38:55 -0400

    Add files via upload

---
+++ HW1_c.py
@@ -0,0 +1,54 @@
+__author__ = 'Mai Bui'
+__version__ = '09/09/2020'
+
+from copy import deepcopy
+
+
+def predict(row, weights):
+    activation = weights[0]
+    for i in range(len(row)-1):
+        activation += weights[i+1] * row[i]
+    return 1 if activation >= 0 else 0
+
+
+def create_table(n):
+    if n < 1:
+        return [[]]
+    table = create_table(n-1)
+    return [row + [i] for row in table for i in [0,1]]
+
+
+def get_and_logic(table):
+    result = deepcopy(table)
+    for row in result:
+        row.append(int(all(row)))
+    return result
+
+
+def get_or_logic(table):
+    result = deepcopy(table)
+    for row in result:
+        row.append(int(any(row)))
+    return result
+
+
+if __name__ == "__main__":
+    n = int(input("Please enter a number: "))
+    dataset = []
+    dataset = create_table(n)
+    dataset_and = get_and_logic(dataset)
+    weights_and = [-n]+[1]*n
+    dataset_or = get_or_logic(dataset)
+    weights_or = [-1]+[1]*n
+
+    print("Create dataset for n =", n, "using AND Logic")
+    print(dataset_and)
+    for row in dataset_and:
+        prediction_and = predict(row, weights_and)
+        print("Expected=%d, Predicted=%d" % (row[-1], prediction_and))
+
+    print("Create dataset for n =", n, "using OR Logic")
+    print(dataset_or)
+    for row in dataset_or:
+        prediction_or = predict(row, weights_or)
+        print("Expected=%d, Predicted=%d" % (row[-1], prediction_or))
